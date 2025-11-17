b576b0a45e873e9d01e293629a6b3a77232a852c
krisamorim <kris.a.afurtado@gmail.com>
2025-11-17 15:43:47 -0300

    Adicionado loop para ficar no codigo até receber resposta valida  apra definição de idioma

--- gitClass.py
+++ gitClass.py
@@ -1,7 +1,10 @@
-choiceLang = input("Escolha o idioma / Choose the language\nPT para português\nEN to English): ").strip().upper()
-if choiceLang == 'PT':
-    print("Este é um arquivo para praticar a aula de git")
-elif choiceLang == 'EN':
-    print("This is a file to practice the git class")
-else:
-    print("Idioma não reconhecido / Language not recognized")
+while True:
+    choiceLang = input("Escolha o idioma / Choose the language\nPT para português\nEN to English): ").strip().upper()
+    if choiceLang == 'PT':
+        print("Este é um arquivo para praticar a aula de git")
+        break
+    elif choiceLang == 'EN':
+        print("This is a file to practice the git class")
+        break
+    else:
+        print("Idioma não reconhecido / Language not recognized. Tente novamente.")
